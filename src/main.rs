64229f00f0005ed9d30cadbfbc82d1490e32e587
JoshCu <Josh.Cu@gmail.com>
2023-04-05 15:40:26 -0400

    added a cube for debug

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use isosurface::marching_cubes::MarchingCubes;
 use isosurface::source::Source;
 use kiss3d::light::Light;
-use kiss3d::nalgebra::{Point3, Vector3};
+use kiss3d::nalgebra::{Point3, UnitQuaternion, Vector3};
 use kiss3d::window::Window;
 use rand::Rng;
 use std::cell::RefCell;
@@ -15,7 +15,16 @@ fn main() {
     let mut window =
         Window::new_with_size("Scientific Visualization", window_size.0, window_size.1);
 
+    ///////////////////////////////
+    let mut c = window.add_cube(1.0, 1.0, 1.0);
+    c.set_color(1.0, 0.0, 0.0);
+    ///////////////////////////////
+
     window.set_light(Light::StickToCamera);
+
+    //////////////////////////////
+    let rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), 0.014);
+    //////////////////////////////
 
     let iso_values = [0.2, 0.4, 0.6, 0.8];
     for &iso_value in iso_values.iter() {
@@ -29,15 +38,21 @@ fn main() {
             .map(|v| Point3::new(v[0], v[1], v[2]))
             .collect::<Vec<_>>();
 
-        // generate a mutable mesh
-        let mut mesh = kiss3d::resource::Mesh::new(vertices, indices, None, None, false);
+        let indices = indices
+            .chunks(3)
+            .map(|i| Point3::new(i[0] as u16, i[1] as u16, i[2] as u16))
+            .collect::<Vec<_>>();
+
+        let mesh = kiss3d::resource::Mesh::new(vertices, indices, None, None, false);
 
         let color = custom_color(iso_value);
-        let mut node = window.add_mesh(Rc::new(RefCell::new(mesh)), Vector3::new(1.0, 1.0, 1.0));
+        let mut node = window.add_mesh(Rc::new(RefCell::new(mesh)), Vector3::new(10.0, 10.0, 10.0));
         node.set_color(color.x, color.y, color.z);
     }
 
-    while window.render() {}
+    while window.render() {
+        // c.prepend_to_local_rotation(&rot);
+    }
 }
 
 struct ScalarField {
@@ -47,13 +62,12 @@ struct ScalarField {
 
 impl Source for ScalarField {
     fn sample(&self, x: f32, y: f32, z: f32) -> f32 {
-        // return the value at point x y z
-        let x = (x * self.dimensions.0 as f32) as usize;
-        let y = (y * self.dimensions.1 as f32) as usize;
-        let z = (z * self.dimensions.2 as f32) as usize;
-        self.data[x
-            + y * self.dimensions.0 as usize
-            + z * self.dimensions.0 as usize * self.dimensions.1 as usize]
+        // Clamp the indices to avoid out of bounds access
+        let x = ((x * self.dimensions.0 as f32) as usize).min(self.dimensions.0 - 1);
+        let y = ((y * self.dimensions.1 as f32) as usize).min(self.dimensions.1 - 1);
+        let z = ((z * self.dimensions.2 as f32) as usize).min(self.dimensions.2 - 1);
+
+        self.data[x + y * self.dimensions.0 + z * self.dimensions.0 * self.dimensions.1]
     }
 }
 
